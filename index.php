e7b7344d3723362bcac3ebbcf7e3cd5fd4fa6495
dvijetel <dvijetel@mail.ru>
2019-03-31 15:37:28 +0300

    added hw

---
+++ index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<!--<br>___________________task1_________________<br>-->
+<?php
+//$str="Halla Bagla and another planet";
+//if (strlen($str)>10) {
+//    $str1=substr($str,0,10);
+//    };
+//$str2=str_replace("a","",$str1);
+//echo $str2;
+//?>
+<!--<br>___________________task2_________________<br>-->
+<?php
+//$str=strlen(strip_tags("<div class=\"refnamediv\">
+//<h1>htmlspecialchars</h1>
+//<p> (PHP 4, PHP 5, PHP 7)</p><p class=\"refpurpose\"><span class=\"refname\">htmlspecialchars</span> —
+//<span class=\"dc-title\">Любой текст</span></p>
+//</div>"));
+//echo ($str);
+//?>
+<!--<br>___________________task3_________________<br>-->
+<?php
+//$str="http://example.com/user/username";
+//$str1=str_replace("user","author",$str);
+//echo $str1;
+//?>
+<!--<br>___________________task4_________________<br>-->
+<?php
+//$arr=[1,22,45,3,23,45,6,76,55,4];
+//sort($arr);
+//echo "<pre>";
+//var_dump($arr);
+//echo "</pre>";
+?>
+<!--<br>___________________task5_________________<br>-->
+<?php
+//$str="Find me and say Hello Mike!";
+//$arr=explode(" ",$str);
+//echo "<pre>";
+//var_dump($arr);
+//echo "</pre>";
+//?>
+<!--<br>___________________task6_________________<br>-->
+<?php
+//$str="Find me and say Hello Mike!";
+//$a=substr_count($str,"a");
+//echo $a;
+//?>
+<!--<br>___________________task7_________________<br>-->
+<?php
+//$url="http://example.com/name=Mike&lastname=Jackson&age=25";
+//parse_str($url,$arr);
+////$arr[0]=substr($arr[0],-4);
+////echo $arr[0];
+//echo "<pre>";
+//var_dump($arr);
+//echo "</pre>";
+//?>
+
+
+</body>
+</html>
